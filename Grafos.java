9936feb4c2d917990edabfda665e6700b6d0f935
Gabriela Silva Do Carmo <31321042+gabscarmo@users.noreply.github.com>
2021-10-06 22:46:24 -0300

    Update Grafos.java

--- Grafos.java
+++ Grafos.java
@@ -34,29 +34,29 @@ public class Grafo<TIPO> {
         return vertice;
     }
     
-    
+    //Solução da equipe
     public void buscaEmLargura(){
         int x = 1; //Iniciamos com x=1 pois iniciamos apartir de um vertice
-        ArrayList<Vertice<TIPO>> marcados = new ArrayList<Vertice<TIPO>>();
-        ArrayList<Vertice<TIPO>> fila = new ArrayList<Vertice<TIPO>>();
-        Vertice<TIPO> atual = vertices.get(0);
-        marcados.add(atual);
+        ArrayList<Vertice<TIPO>> marcados = new ArrayList<Vertice<TIPO>>(); //Lista para guardar os vértices que já foram contabilizados
+        ArrayList<Vertice<TIPO>> fila = new ArrayList<Vertice<TIPO>>();//Fila para guardaro caminho percorrido
+        Vertice<TIPO> atual = vertices.get(0); // Vertice de início
+        marcados.add(atual);//adicionar o vértice atual à lista de marcados
         System.out.println(atual.getDado());
-        fila.add(atual);
+        fila.add(atual);//adicionar a fila do caminho percorrido
         while(fila.size() > 0){
             Vertice<TIPO> visitado = fila.get(0);
             for(int i=0; i < visitado.getArestasSaida().size(); i++){
                 Vertice<TIPO> proximo = visitado.getArestasSaida().get(i).getFim();
-                if (!marcados.contains(proximo)){ //se o vértice ainda não foi marcado
-                    marcados.add(proximo);
+                if (!marcados.contains(proximo)){ //boleano para saber se a lista de marcados contém o vértice próximo
+                    marcados.add(proximo);//adicina o vértice à lista de marcados
                     System.out.println(proximo.getDado());
-                    fila.add(proximo);
-                    x= 1 + x;
+                    fila.add(proximo); //adiciona o o vértice à fila
+                    x= 1 + x; //incrementa valor de vértices conectados
                 }
             }
-            fila.remove(0);
+            fila.remove(0);//remove o valor inicial da fila
         }
-        System.out.println(x);
+        System.out.println(x);//Saída do valor de vértices conectados
     }
 
 }
